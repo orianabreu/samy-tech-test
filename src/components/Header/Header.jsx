03d86a59c17cb9499e294c5b4fc8d689b7b628b0
Oriana Abreu <oriabreu.v@gmail.com>
2022-07-18 17:54:24 +0200

    create input component, add it to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Input from '../Input/Input';
 
 import Logo from '../Logo/Logo';
 
 export default function Header() {
   return (
     <header className="header">
-        <Logo />
+      <Logo />
+      <Input
+        type="text"
+        placeholder="You're looking for something?"
+        className="input input--search"
+      />
     </header>
-  )
+  );
 }
