5e3c706405e9e8b2356f6b61e4444f0338d8d346
Oriana Abreu <oriabreu.v@gmail.com>
2022-07-19 21:59:01 +0200

    call endpoint from main comp and render cards

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Container from '../../components/Container/Container';
+import Card from '../../components/Card/Card';
 
 export default function Main() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3100/images').then((response) => {
+      console.log(response.data);
+      setImages(response.data);
+    });
+    return () => {};
+  }, []);
+
   return (
-    <Container></Container>
-  )
+    <Container>
+      <div className="grid">
+        {images.map((image) => {
+          return (
+            <Card
+              key={image.id}
+              price={image.price}
+              image={image.main_attachment.small}
+              title={image.title}
+              author={image.author}
+              likesCount={image.likes_count}
+            />
+          );
+        })}
+      </div>
+    </Container>
+  );
 }
